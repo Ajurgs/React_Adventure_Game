2540f6d564306e83d69345e5f07ef9fed4231fc2
Jared Sutch <sutch002@gmail.com>
2021-06-30 23:28:03 -0500

    made functionality to the buttons in characterjs

--- client/src/pages/Character.js
+++ client/src/pages/Character.js
@@ -1,47 +1,56 @@
 import React from "react";
 // import {Link} from 'react-router-dom';
 // import GameScreen from "../components/GameScreen";
-import { QUERY_CHARACTERS } from "../utils/queries";
+import { QUERY_CHARACTERS, QUERY_SINGLE_CHARACTER } from "../utils/queries";
 import { useQuery } from "@apollo/client";
-
+import { useMutation } from "@apollo/client";
+import { ADD_CHARACTER_TO_PROFILE } from "../utils/mutations";
 //import quries and mutations?
 
 // import Auth from '../../utils/auth';
 
-const Character = ({ characters
+const Character = () => {
+  const { loading, data: character } = useQuery(QUERY_CHARACTERS);
 
-  // class,
-}) => {
-  const { loading, data : character } = useQuery(QUERY_CHARACTERS);
-
-  //  => {
-  //     if(!character.length) {
-  //         return <h3>Choose a Character please!</h3>;
-  //     }
+  const [addCharacterToProfile, { error, data }] = useMutation(
+    ADD_CHARACTER_TO_PROFILE
+  );
+  const addCharacter = async (characterId) => {
+    try {
+      // const { data } = await addCharacterToProfile({
+      //   variables: { characterId },
+      // });
+      console.log(characterId);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   if (loading) {
     return <div>Loading...</div>;
-  };
+  }
 
-  console.log(character)
+  console.log(character);
   return (
     <div>
       <h3> Characters</h3>
-       <main className="flex-row justify-start">
+      <main className="flex-row justify-start">
         <div className="col-12 col-md-10 mb-5">
-          {characters && characters.map((hero, index)=> (
-          <div key = {character.id} className ="card mb-3">
-<h4 className="card-header">
-{character.name}<br/>
-{character.health}<br/>
-           Zach</h4>
-          </div>
-            
+          {character.characters.map((hero, index) => (
+            <div key={index} className="card mb-3">
+              <div className="card-header">
+                <h4>Name:{hero.name}</h4>
+                <h4>Health: {hero.health}</h4>
+                <h4>Attack: {hero.attack}</h4>
+                <h4>Cost: {hero.cost}</h4>
+              </div>
+              <div className="card-body">
+                <button onClick={() => addCharacter(hero._id)}>Buy</button>
+              </div>
+            </div>
           ))}
-          </div>  
-    
-      
-      </main> 
-    </div> 
+        </div>
+      </main>
+    </div>
   );
 };
 export default Character;
